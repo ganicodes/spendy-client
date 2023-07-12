da6d3d763ccaf1ae171cf58bfc37813091d112aa
Gani <abdulgani.work@gmail.com>
2023-07-13 01:40:28 +0530

    feature - added edit functionality to expense records

---
+++ src/common/columnsConstant.js
@@ -0,0 +1,42 @@
+export const columnConstants = {
+    expenseGridColumns: [
+        {
+            id: "date",
+            header: "Date",
+        },
+        {
+            id: "amount",
+            header: "Amount",
+        },
+        {
+            id: "type",
+            header: "Type",
+        },
+        {
+            id: "category",
+            header: "Category",
+        },
+        {
+            id: "description",
+            header: "Description",
+        },
+    ],
+    incomeGridColumns: [
+        {
+            id: "date",
+            header: "Date",
+        },
+        {
+            id: "amount",
+            header: "Amount",
+        },
+        {
+            id: "source",
+            header: "Source",
+        },
+        {
+            id: "description",
+            header: "Description",
+        },
+    ]
+}
--- src/pages/ledger/container/Expense.jsx
+++ src/pages/ledger/container/Expense.jsx
@@ -2,33 +2,13 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BeatLoader } from "react-spinners";
 import { APIURLConstant } from "../../../common/apiURLConstant";
+import { columnConstants } from "../../../common/columnsConstant";
 import { DataGrid } from "../../../components/reusable/data-grid/DataGrid";
 import APIHelper from "../../../helper/APIHelper";
 import { setShowAlert } from "../../../store/alert";
 import ExpenseForm from "../components/ExpenseForm";
 
-const columns = [
-  {
-    id: "date",
-    header: "Date",
-  },
-  {
-    id: "amount",
-    header: "Amount",
-  },
-  {
-    id: "type",
-    header: "Type",
-  },
-  {
-    id: "category",
-    header: "Category",
-  },
-  {
-    id: "description",
-    header: "Description",
-  },
-];
+const columns = columnConstants.expenseGridColumns;
 
 const Expense = () => {
   const { userId } = useSelector((state) => state.user);
@@ -36,9 +16,11 @@ const Expense = () => {
 
   const [gridData, setGridData] = useState([]);
   const [showLoader, setShowLoader] = useState(false);
+  const [editExpenseData, setEditExpenseData] = useState(null);
 
   useEffect(() => {
     getExpenseList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getExpenseList = async () => {
@@ -57,8 +39,9 @@ const Expense = () => {
   };
 
   const onEdit = (data) => {
-    console.log(data);
+    setEditExpenseData(data);
   };
+
   const onDelete = async (rowData) => {
     const id = rowData._id;
     const { data } = await APIHelper.delete(
@@ -74,14 +57,16 @@ const Expense = () => {
         })
       );
     }
-    console.log("data: ", data);
   };
 
   return (
     <div className="mx-4">
-      {/* Filter and search and an add button */}
-      <div className=" mb-8">
-        <ExpenseForm getExpenseList={getExpenseList} />
+      <div className="mb-8">
+        <ExpenseForm
+          getExpenseList={getExpenseList}
+          editExpenseData={editExpenseData}
+          resetEditExpenseData={() => setEditExpenseData(null)}
+        />
       </div>
       {showLoader ? (
         <div className="absolute left-1/2 top-1/2 translate-x-1/2 translate-y-1/2">
@@ -90,7 +75,9 @@ const Expense = () => {
       ) : (
         <div className="max-h-[50vh] overflow-y-scroll">
           {gridData.length === 0 ? (
-            <p className="w-full text-center text-primary">Add data to view</p>
+            <p className="w-full text-center text-primary">
+              No records to display
+            </p>
           ) : (
             <DataGrid
               gridData={gridData}
